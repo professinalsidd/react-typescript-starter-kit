2e603646939b63dc4dfb409e96efb57e369828d7
professinalsidd <professionalsiddmail@gmail.com>
2024-11-11 12:35:28 +0530

    feat: add nav bar

---
+++ src/container/screens/home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import NavBarComp from "../../../components/navbar/navbar";
+
+const HomeScreen = () => {
+  return (
+    <Fragment>
+      <NavBarComp />
+    </Fragment>
+  );
+};
+
+export default HomeScreen;
