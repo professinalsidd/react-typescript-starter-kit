2ae8445d9e8c52f7cac384a8c48ba26f8384be4f
professinalsidd <professionalsiddmail@gmail.com>
2024-11-11 17:35:37 +0530

    remove : extra menu

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,22 +1,11 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import { navBar } from "../../db";
 import { COLORS } from "../../themes/themes";
 
 const style = {
   mr: 2,
-  display: "flex",
-  flexGrow: 1,
-  fontFamily: "monospace",
   fontWeight: 900,
   textDecoration: "none",
   background: COLORS.RED_BLACK,
@@ -25,18 +14,6 @@ const style = {
 };
 
 function NavBarComp() {
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
     <AppBar position="static" sx={{ background: COLORS.MediumWhite }}>
       <Container maxWidth="xl">
@@ -48,40 +25,8 @@ function NavBarComp() {
             href="#app-bar-with-responsive-menu"
             sx={style}
           >
-            Todo
+            Todo List
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {navBar.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
         </Toolbar>
       </Container>
     </AppBar>
